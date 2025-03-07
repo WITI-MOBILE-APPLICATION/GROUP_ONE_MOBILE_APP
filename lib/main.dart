e8d29a18f53f9f33b8aefde20970fb2a1291dc26
shanitahgithub <nakaggshanitah874@gmail.com>
2025-03-07 16:33:20 +0300

    Group one

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,39 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import './services/movie_services.dart';
-// import 'providers/movie_provider.dart';
-// import 'screens/home_screen.dart';
-// import './screens/movie_detail_screen.dart';
-// import 'screens/splash_screen.dart';
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await dotenv.load(fileName: "assets/.env");
-
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: (_) => MovieProvider()),
-//       ],
-//       child: MaterialApp(
-//         title: 'MovieVault',
-//         theme: ThemeData.dark().copyWith(
-//           primaryColor: Colors.blueAccent,
-//           visualDensity: VisualDensity.adaptivePlatformDensity,
-//         ),
-//         debugShowCheckedModeBanner: false,
-//         initialRoute: '/',
-//         routes: {
-//           '/': (context) => SplashScreen(),
-//           '/home': (context) => HomeScreen(),
-//         },
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import './services/movie_services.dart';
+import 'providers/movie_provider.dart';
 import 'screens/home_screen.dart';
+import './screens/movie_detail_screen.dart';
+import 'screens/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MovieProvider()),
+      ],
+      child: MaterialApp(
+        title: 'MovieVault',
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.blueAccent,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => SplashScreen(),
+          '/home': (context) => HomeScreen(),
+        },
+      ),
     );
   }
 }
