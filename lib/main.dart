c76fa1432bf12965f3c33dc29240ce26213d5d09
shanitahgithub <nakaggshanitah874@gmail.com>
2025-03-12 10:47:07 +0300

    Splash ,auth_screens, homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/forgot_password.dart';
+import 'package:my_app/screens/signup_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import './services/movie_services.dart';
-import 'providers/movie_provider.dart';
 import 'screens/home_screen.dart';
-import './screens/movie_detail_screen.dart';
+import 'screens/movie_detail_screen.dart';
 import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'providers/movie_provider.dart';
+import 'screens/forgot_password.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,15 +27,25 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'MovieVault',
+        // theme: ThemeData.dark().copyWith(
+        //   primaryColor: Colors.blueAccent,
+        //   // primaryColor: Colors.black,
+        //   visualDensity: VisualDensity.adaptivePlatformDensity,
+        // ),
         theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.blueAccent,
+          primaryColor: Color(0xFF06041F), // Custom Black Pearl color
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+
         debugShowCheckedModeBanner: false,
-        initialRoute: '/',
+        initialRoute: '/', // Start with the splash screen
         routes: {
           '/': (context) => SplashScreen(),
+          // Login screen route
+          '/signup': (context) => SignUpScreen(),
+          '/login': (context) => LoginScreen(),
           '/home': (context) => HomeScreen(),
+          '/forgot': (context) => ForgotPasswordScreen()
         },
       ),
     );
